1980bf0284881cab1dec267db8a5f120ecc0e908
Anirban Paul <anirbanpaul56@gmail.com>
2025-05-04 16:07:44 +0530

    new programs added

---
+++ Heap/InsertionMax.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MaxHeap {
+private:
+    vector<int> heap;
+
+    void heapifyUp(int index) {
+        int parent = (index - 1) / 2;
+        if (index > 0 && heap[index] > heap[parent]) {
+            swap(heap[index], heap[parent]);
+            heapifyUp(parent);
+        }
+    }
+
+public:
+    void insert(int value) {
+        heap.push_back(value);        // Step 1: Add to the end
+        heapifyUp(heap.size() - 1);   // Step 2: Restore heap property
+    }
+
+    void display() {
+        cout << "Heap array: ";
+        for (int val : heap)
+            cout << val << " ";
+        cout << endl;
+    }
+};
+
+int main() {
+    MaxHeap h;
+    h.insert(10);
+    h.insert(20);
+    h.insert(5);
+    h.insert(30);
+    h.insert(60);
+    h.display();  // Output: Heap array: 30 20 5 10
+    return 0;
+}
